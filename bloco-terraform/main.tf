f72f822c78e34b571d201ec0776e23bf3b1f5f31
Marcos Teixeira <marcos.teixeira@nscreen.com.br>
2024-07-17 11:25:34 -0300

    chore: Add initial Terraform configuration file

---
+++ bloco-terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.3.0"
+
+  required_providers {
+    aws = {
+        version = "1.0"
+        source = "hashicorp/aws"
+        }
+
+    azurerm = {
+        version = "2.0"
+        source = "hashicorp/azurerm"
+        }
+    }   
+
+
+    backend "azurerm" {
+  resource_group_name   = "rg-terraform"
+  storage_account_name  = "saterraform"
+  container_name        = "tfstate"
+  key                   = "terraform.tfstate"
+    }
+}
